19b88d21af71f8663f97189fd152ad19616d7949
Luiz-Eduardo-BL <limadudu34@gmail.com>
2024-11-18 14:42:10 -0300

    ✨ feat: remover código comentado da função de cálculo de média no componente Home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -50,12 +50,6 @@ const Home = () => {
     fetchJogadores();
   }, []);
 
-  // const calcularMedia = (votos: { userId: string; vote: number }[] | undefined) => {
-  //   if (!votos || votos.length === 0) return 0;
-  //   const total = votos.reduce((acc, voto) => acc + voto.vote, 0);
-  //   return total / votos.length;
-  // };
-
   const calcularMedia = (votos: { userId: string; vote: number }[] | undefined) => {
     if (!votos || votos.length === 0) return 0;
     const votosValidos = votos.filter((voto) => typeof voto === "object" && voto.vote !== undefined);
