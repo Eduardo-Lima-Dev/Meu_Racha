555475a84e28da335f8b4db2868003f0591622dc
Luiz-Eduardo-BL <limadudu34@gmail.com>
2025-01-07 18:46:26 -0300

    🔧 chore: adicionar suporte a alias "@" no Jest e configurar ambiente de testes

--- jest.config.ts
+++ jest.config.ts
@@ -6,10 +6,12 @@ const config: Config = {
   transform: {
     '^.+\\.tsx?$': 'babel-jest',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1', // Adiciona suporte ao alias "@"
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mapeia arquivos CSS
   },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
 export default config;
