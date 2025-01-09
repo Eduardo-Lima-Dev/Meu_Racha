ad8c8217d442d604d9c38c757c6aedff948b7c4d
Luiz-Eduardo-BL <limadudu34@gmail.com>
2025-01-09 09:32:54 -0300

    🔧 chore: refatorar lógica de cálculo de estatísticas dos jogadores e otimizar a recuperação de dados

--- src/app/graficos/hooks/useJogadoresEstatisticas.tsx
+++ src/app/graficos/hooks/useJogadoresEstatisticas.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { ref, get } from "firebase/database";
 import { database } from "../../../config/firebaseConfig";
 import { EstatisticasJogador } from "../types";
+import { calcularMedia } from "../utils/calcularMedia";
 
 export const useJogadoresEstatisticas = () => {
   const [jogadores, setJogadores] = useState<EstatisticasJogador[]>([]);
@@ -10,32 +11,45 @@ export const useJogadoresEstatisticas = () => {
   const [error, setError] = useState("");
 
   useEffect(() => {
+
     const fetchJogadores = async () => {
       setLoading(true);
       setError("");
-
+    
       try {
         const dbRef = ref(database, "jogadores");
         const snapshot = await get(dbRef);
-
+    
         if (snapshot.exists()) {
           const data = snapshot.val();
           const listaJogadores: EstatisticasJogador[] = Object.keys(data).map((key) => {
             const notas = data[key].notas || {};
+            const quantidadeNotas = Object.keys(notas).length;
+    
+            const somatorioNotas = Object.values(notas).reduce(
+              (acc: { [key: string]: number }, usuarioNotas: any) => {
+                Object.keys(usuarioNotas).forEach((estatistica) => {
+                  acc[estatistica] = (acc[estatistica] || 0) + usuarioNotas[estatistica];
+                });
+                return acc;
+              },
+              {}
+            );
+    
             return {
               id: key,
               nome: data[key].nome || "",
-              fisico: notas.fisico || 0,
-              finalizacao: notas.finalizacao || 0,
-              drible: notas.drible || 0,
-              defesa: notas.defesa || 0,
-              passe: notas.passe || 0,
-              quantidadeNotas: Object.keys(notas).length,
+              fisico: calcularMedia(somatorioNotas["fisico"], quantidadeNotas),
+              finalizacao: calcularMedia(somatorioNotas["finalizacao"], quantidadeNotas),
+              drible: calcularMedia(somatorioNotas["drible"], quantidadeNotas),
+              defesa: calcularMedia(somatorioNotas["defesa"], quantidadeNotas),
+              passe: calcularMedia(somatorioNotas["passe"], quantidadeNotas),
+              quantidadeNotas,
             };
           });
-
+    
           setJogadores(listaJogadores);
-
+    
           if (listaJogadores.length > 0) {
             setJogadorSelecionado(listaJogadores[0]);
           }
@@ -48,7 +62,7 @@ export const useJogadoresEstatisticas = () => {
       } finally {
         setLoading(false);
       }
-    };
+    };    
 
     fetchJogadores();
   }, []);
